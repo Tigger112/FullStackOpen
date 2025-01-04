fe57b11a5739e4df2f773a1f64cb03fa08645fce
tigger <tiggerjeo@gmail.com>
2025-01-04 12:42:05 -0600

    did exercises 1.1 and 1.2. added new components to the site

---
+++ part1/coures-information/src/App.jsx
@@ -0,0 +1,58 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0]} exercise={props.exercises[0]}/>
+      <Part part={props.parts[1]} exercise={props.exercises[1]}/>
+      <Part part={props.parts[2]} exercise={props.exercises[2]}/>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercise}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises {props.exercises[0] + props.exercises[1] + props.exercises[2]}
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    'Fundamentals of React', 
+    'Using props to pass data', 
+    'State of a component'
+  ]
+  const exercises = [10, 7, 14]
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} exercises={exercises} />
+      <Total exercises={exercises} />
+    </div>
+  )
+}
+
+export default App
